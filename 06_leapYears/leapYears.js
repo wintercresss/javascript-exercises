f5feb8b47aa8163f2a178e43b19b76f897ee8b42
wintercress <liho.mak11@gmail.com>
2023-11-20 06:56:47 -0600

    solved question 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function(num) {
+    if (num % 400 == 0) {
+        return true
+    }
+    if (num % 4 == 0 && num % 100 != 0) {
+        return true
+    }
+    return false
 };
 
 // Do not edit below this line
