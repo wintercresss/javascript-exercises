f0bcf4dc6bb0488b3caf455331141396f70b1101
wintercress <liho.mak11@gmail.com>
2023-11-20 06:51:25 -0600

    solved question 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,28 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (num1 < 0 || num2 < 0) {
+        return "ERROR"
+    }
+
+    if (!(typeof num1 == "number")) return "ERROR"
+    if (!(typeof num2 == "number")) return "ERROR"
+
+    var lower = 0
+    var higher = 0
+    var sum = 0
+
+    if (num1 < num2) {
+        lower = num1
+        higher = num2
+    } else {
+        lower = num2
+        higher = num1
+    }
+
+    for (let i = lower; i <= higher; i++) {
+        sum += i
+    }
+
+    return sum
 
 };
 
