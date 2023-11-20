eb2789bb9e8b3f6e823ecd3400282fd5d2bc33b7
wintercress <liho.mak11@gmail.com>
2023-11-20 06:16:06 -0600

    solved question 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    var toreturn = ""
+    for (let i = string.length-1; i >= 0; i--) {
+        toreturn += string[i]
+    }
+    return toreturn
 };
 
 // Do not edit below this line
