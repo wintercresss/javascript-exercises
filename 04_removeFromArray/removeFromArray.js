a834d1db6998bcab921c43a18ee0e5af2a831fe1
wintercress <liho.mak11@gmail.com>
2023-11-20 06:37:50 -0600

    solved question 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+    var ret = []
+    var found = false
+    for (let i = 0; i < arr.length; i++) {
+        found = false
+        for (let j = 0; j < args.length; j++){
+            if (arr[i] === args[j]) {
+                found = true
+                break
+            }
+        }
+        if (found == false) {
+            ret.push(arr[i])
+        }
+    }
+    console.log(ret)
+    return ret
 };
 
 // Do not edit below this line
